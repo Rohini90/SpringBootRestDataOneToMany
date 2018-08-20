abf6fb752a1b0f3feb1c92613a8065651c034e01
Rohini Joshi <rohini.joshi@digitalcues.com>
2018-08-20 10:57:31 +0530

    First commit

---
+++ SpringBootRestDataOneToMany-1/src/main/java/com/digitalcues/springrest/SpringBootRestDataOneToMany1Application.java
@@ -0,0 +1,12 @@
+package com.digitalcues.springrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestDataOneToMany1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestDataOneToMany1Application.class, args);
+	}
+}
