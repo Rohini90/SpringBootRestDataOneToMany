971a4f2c9bf4ffc3a8a281fefd3500c1b50d5eab
Rohini Joshi <rohini.joshi@digitalcues.com>
2018-08-21 10:10:44 +0530

    SpringBootRestDataOneToManyApplicationTest.java

---
+++ SpringBootRestDataOneToMany-1/src/test/java/com/digitalcues/springBootRestDataOneToMany/SpringBootRestDataOneToMany1ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.digitalcues.springBootRestDataOneToMany;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootRestDataOneToMany1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
