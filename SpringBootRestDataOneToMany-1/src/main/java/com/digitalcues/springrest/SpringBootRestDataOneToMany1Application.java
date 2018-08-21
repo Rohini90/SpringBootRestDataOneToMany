ac0e8cd0217872f128cd39c93d70973c0c9c7ef0
Rohini Joshi <rohini.joshi@digitalcues.com>
2018-08-21 10:19:08 +0530

    Application file

--- SpringBootRestDataOneToMany-1/src/main/java/com/digitalcues/springrest/SpringBootRestDataOneToMany1Application.java
+++ SpringBootRestDataOneToMany-1/src/main/java/com/digitalcues/springrest/SpringBootRestDataOneToMany1Application.java
@@ -2,8 +2,12 @@ package com.digitalcues.springrest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@ComponentScan({"com.digitalcues.controller","com.digitalcues.model"})
+@EnableMongoRepositories("com.digitalcues.repository")
 public class SpringBootRestDataOneToMany1Application {
 
 	public static void main(String[] args) {
