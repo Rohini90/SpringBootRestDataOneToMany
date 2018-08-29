9c0553334695f957149a1782c3eb59db434fb3be
Rohini Joshi <rohini.joshi@digitalcues.com>
2018-08-29 17:28:25 +0530

    Modified
    SpringBootRestDataOneToMany1Application.java

--- SpringBootRestDataOneToMany-1/src/main/java/com/digitalcues/springrest/SpringBootRestDataOneToMany1Application.java
+++ SpringBootRestDataOneToMany-1/src/main/java/com/digitalcues/springrest/SpringBootRestDataOneToMany1Application.java
@@ -2,16 +2,16 @@ package com.digitalcues.springrest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.PropertySource;
+
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication//(exclude = DataSourceAutoConfiguration.class)
 //@ComponentScan("com.digitalcues.model")
-@ComponentScan({"com.digitalcues.controller","com.digitalcues.model","com.digitalcues.service"})
+
+@ComponentScan({"com.digitalcues.controller","com.digitalcues.model","com.digitalcues.service","com.digitalcues.assemblers"})
 @EnableMongoRepositories("com.digitalcues.repository")
-@PropertySource("classpath:application.properties")
+//@PropertySource("classpath:application.properties")
 public class SpringBootRestDataOneToMany1Application {
 
 	public static void main(String[] args) {
