2c01b89fe2546a9828dfefa539790fcec548bc65
palak sharma <spalak7@gmail.com>
2018-08-30 12:01:06 +0530

    modify Person.java add equals and hashcode methods

--- SpringBootRestDataOneToMany-1/src/main/java/com/digitalcues/model/Person.java
+++ SpringBootRestDataOneToMany-1/src/main/java/com/digitalcues/model/Person.java
@@ -152,4 +152,31 @@ public class Person {
 				+ ", createdOn=" + createdOn + ", updatedOn=" + updatedOn + ", address=" + address + "]";
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((personId == null) ? 0 : personId.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		if (personId == null) {
+			if (other.personId != null)
+				return false;
+		} else if (!personId.equals(other.personId))
+			return false;
+		return true;
+	}
+
+	
+	
 }
